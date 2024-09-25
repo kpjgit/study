be69397521f068a18c44fc1d726a8be95ea2c119
kpjgit <thisisdisposableid001@gmail.com>
2024-09-25 17:23:07 +0900

    .

--- baek/simulation/sticker.cpp
+++ baek/simulation/sticker.cpp
@@ -1,78 +1,65 @@
 //18808
 #include <bits/stdc++.h>
 
-int n, m, k;
-bool notebook[50][50];
-int r, c;
+int N, M, K;
+int R, C;
+bool laptop[40][40];
 bool sticker[10][10];
 
-bool can_stick(int x, int y) {
-    for(int i = 0; i < r; i++) {
-        for(int j = 0; j < c; j++) {
-            if(notebook[x + i][y + j] == 1 && sticker[i][j] == 1) return false; 
+bool stick(int x, int y) {
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
+            if(laptop[x + i][y + j] == true && sticker[i][j] == true) return false;
         }
     }
-    for(int i = 0; i < r; i++) {
-        for(int j = 0; j <c; j++) {
-            if(sticker[i][j] == 1) notebook[x + i][y + j] = 1; 
-        }
+    
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) if(sticker[i][j] == true) laptop[x + i][y + j] = true;
     }
-
     return true;
 }
 
 void rotate() {
-    bool temp[10][10];
-    for(int i = 0; i < r; i++) {
-        for(int j = 0; j <c; j++) {
-            temp[i][j] = sticker[i][j];
-        }
+    int temp[10][10];
+    for(int i = 0; i < C; i++) {
+        for(int j = 0; j < R; j++) temp[i][j] = sticker[R - j - 1][i];
     }
-
-    for(int i = 0; i < c; i++) {
-        for(int j = 0; j < r; j++) {
-            sticker[i][j] = temp[r - 1 - j][i];
-        }
+    
+    std::swap(R, C);
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) sticker[i][j] = temp[i][j];
     }
-
-    std::swap(r, c);
 }
 
 int main(void) {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    std::cin >> n >> m >> k;
-
-    while(k--) {
-        std::cin >> r >> c;
-        for(int i = 0; i < r; i++) {
-            for(int j = 0; j < c; j++) {
-                std::cin >> sticker[i][j];
-            }
+    std::cin >> N >> M >> K;
+    for(int num = 0; num < K; num++) {
+        std::cin >> R >> C;
+        for(int i = 0; i < R; i++) {
+            for(int j = 0; j < C; j++) std::cin >> sticker[i][j];
         }
 
         for(int rot = 0; rot < 4; rot++) {
-            bool did = false;
-            for(int i = 0; i <= n - r; i++) {
-                if(did) break;
-                for(int j = 0; j <= m - c; j++) {
-                    if(can_stick(i, j)) {
-                        did = true;
-                        break;
-                    }
+            bool escape = false;
+            for(int i = 0; i <= N - R; i++) {
+                for(int j = 0; j <= M - C; j++) {
+                    escape = stick(i, j);
+                    if(escape == true) break;
                 }
+                if(escape == true) break;
             }
-            if(did) break;
+            if(escape == true) break;
             rotate();
         }
     }
 
     int ans = 0;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            ans += notebook[i][j];
-        }
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) if(laptop[i][j] == true) ans++;
     }
-    std::cout << ans << '\n';
+
+    std::cout << ans;
 }
