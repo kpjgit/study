8fc639fe26b26fee8ae74e8d140ea74357243f4c
kpjgit <thisisdisposableid001@gmail.com>
2024-09-26 16:13:05 +0900

    .

--- baek/simulation/2048 (easy).cpp
+++ baek/simulation/2048 (easy).cpp
@@ -1,69 +1,92 @@
 //12100
 #include <bits/stdc++.h>
 
-int board[21][21];
-int board2[21][21];
-bool merged[21][21];
-int n;
+int N;
+int input[20][20];
+int board[20][20];
 
-int dx[] = {1, 0, -1, 0};
-int dy[] = {0, 1, 0, -1};
+void func(int dir) {
+    int idx;
+    if(dir == 0) { //오른쪽으로 기울이기
+        for(int i = 0; i < N; i++) {
+            idx = N - 1;
+            int temp[20] = {};
+            for(int j = N - 1; j >= 0; j--) {
+                if(board[i][j] == 0) continue; //옮길게 없음
 
-void move(int curx, int cury, int dir);
-void select(int dir) {
-    if(dir == 0) {
-        for(int i = n - 1; i >= 0; i--) {
-            for(int j = 0; j < n; j++) {
-                move(i, j, dir);
+                if(temp[idx] == 0) { //자리가 비어있음
+                    temp[idx] = board[i][j];
+                } else if(temp[idx] == board[i][j]) { //합치기
+                    temp[idx] *= 2;
+                    idx--;
+                } else {
+                    idx--;
+                    temp[idx] = board[i][j];
+                }
             }
+
+            for(int j = N - 1; j >= 0; j--) board[i][j] = temp[j];
         }
-    }
+    } else if(dir == 1) { //아래로 기울이기
+        for(int j = 0; j < N; j++) {
+            idx = N - 1;
+            int temp[20] = {};
+            for(int i = N - 1; i >= 0; i--) {
+                if(board[i][j] == 0) continue;
 
-    if(dir == 1) {
-        for(int i = 0; i < n; i++) {
-            for(int j = n - 1; j >= 0; j--) {
-                move(i, j, dir);
+                if(temp[idx] == 0) {
+                    temp[idx] = board[i][j];
+                } else if(temp[idx] == board[i][j]) {
+                    temp[idx] *= 2;
+                    idx--;
+                } else {
+                    idx--;
+                    temp[idx] = board[i][j];
+                }
             }
+
+            for(int i = N - 1; i >= 0; i--) board[i][j] = temp[i];
         }
-    }
+    } else if(dir == 2) {
+        for(int i = 0; i < N; i++) {
+            idx = 0;
+            int temp[20] = {};
+            for(int j = 0; j < N; j++) {
+                if(board[i][j] == 0) continue;
 
-    if(dir == 2) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                move(i, j, dir);
+                if(temp[idx] == 0) {
+                    temp[idx] = board[i][j];
+                } else if(temp[idx] == board[i][j]) {
+                    temp[idx] *= 2;
+                    idx++;
+                } else {
+                    idx++;
+                    temp[idx] = board[i][j];
+                }
             }
+
+            for(int j = 0; j < N; j++) board[i][j] = temp[j];
         }
-    }
+    } else if(dir == 3) {
+        for(int j = 0; j < N; j++) {
+            idx = 0;
+            int temp[20] = {};
+            for(int i = 0; i < N; i++) {
+                if(board[i][j] == 0) continue;
 
-    if(dir == 3) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                move(i, j, dir);
+                if(temp[idx] == 0) {
+                    temp[idx] = board[i][j];
+                } else if(temp[idx] == board[i][j]) {
+                    temp[idx] *= 2;
+                    idx++;
+                } else {
+                    idx++;
+                    temp[idx] = board[i][j];
+                }
             }
+            
+            for(int i = 0; i < N; i++) board[i][j] = temp[i];
         }
-    }
-}
-
-void move(int curx, int cury, int dir) {
-    int x = curx;
-    int y = cury;
-    while(1) {
-        x += dx[dir];
-        y += dy[dir];
-        if(x < 0 || x >= n || y < 0 || y >= n) break;
-        if(board2[x][y] == 0) {
-            board2[x][y] = board2[curx][cury];
-            board2[curx][cury] = 0;
-            curx = x;
-            cury = y;
-        } else if(board2[x][y] == board2[curx][cury]) {
-            if(merged[x][y] == 0) {
-                board2[x][y] *= 2;
-                board2[curx][cury] = 0;
-                merged[x][y] = 1;
-            }
-            break;
-        } else break;
     }
 }
 
@@ -71,41 +94,30 @@ int main(void) {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    int ans = 0;
-
-    std::cin >> n;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            std::cin >> board[i][j];
-        }
+    std::cin >> N;
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) std::cin >> input[i][j];
     }
 
-    for(int temp = 0; temp < 1 << (2 * 5) ; temp++) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                board2[i][j] = board[i][j];
-            }
+    int ans = 0;
+    for(int num = 0; num < (1 << (2 * 5)); num++) {
+        for(int i = 0; i < N; i++) { //초기화
+            for(int j = 0; j < N; j++) board[i][j] = input[i][j];
         }
 
-        int brute = temp;
+        int brute = num;
         for(int i = 0; i < 5; i++) {
             int dir = brute % 4;
             brute /= 4;
-            select(dir);
-
-            for(int j = 0; j < n; j++) {
-                for(int k = 0; k < n; k++) {
-                    merged[j][k] = 0;
-                }
-            }
+            func(dir);
         }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                ans = std::max(ans, board2[i][j]);
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(board[i][j] > ans) ans = board[i][j];
             }
         }
     }
-
-    std::cout << ans << '\n';
+    
+    std::cout << ans;
 }
--- baek/simulation/chicken delivery.cpp
+++ baek/simulation/chicken delivery.cpp
@@ -1,41 +1,42 @@
 //15686
 #include <bits/stdc++.h>
 
-int n, m;
-int city[51][51];
+int N, M;
 
 std::vector<std::pair<int, int>> chicken;
 std::vector<std::pair<int, int>> house;
-
 
 int main(void){
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    std::cin >> n >> m;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            std::cin >> city[i][j];
-            if(city[i][j] == 1) house.push_back({i, j});
-            if(city[i][j] == 2) chicken.push_back({i, j});
+    int input;
+    std::cin >> N >> M;
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            std::cin >> input;
+            if(input == 1) house.push_back({i, j});
+            if(input == 2) chicken.push_back({i, j});
         }
     }
 
-    std::vector<int> brute(chicken.size(), 1);
-    std::fill(brute.begin(), brute.begin() + chicken.size() - m, 0);
-    int ans = 300000;
-
+    std::vector<bool> brute(chicken.size(), true);
+    std::fill(brute.begin(), brute.begin() + chicken.size() - M, false); //ture는 폐업
+    
+    int ans = 10000;
     do {
         int dist = 0;
-        for(auto h: house) {
-            int temp = 300000;
+        for(std::pair<int, int> h : house) {
+            int temp = 100;
+
             for(int i = 0; i < chicken.size(); i++) {
-                if(brute[i] == 0) continue;
+                if(brute[i] == false) continue;
                 temp = std::min(temp, std::abs(chicken[i].first - h.first) + std::abs(chicken[i].second - h.second));
             }
             dist += temp;
         }
         ans = std::min(ans, dist);
     } while(std::next_permutation(brute.begin(), brute.end()));
-    std::cout << ans << '\n';
+
+    std::cout << ans;
 }
