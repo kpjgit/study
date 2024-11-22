a9e6acf33c4b4160b46a9c8c95fe19d16e87d883
kpjgit <thisisdisposableid001@gmail.com>
2024-11-22 14:50:38 +0900

    .

--- baek/minimum spanning tree/irrigating.cpp
+++ baek/minimum spanning tree/irrigating.cpp
@@ -1,53 +1,49 @@
 //1368
 #include <bits/stdc++.h>
 
-int n;
-int v, e;
-std::vector<std::pair<int, int>> adj[301];
-bool check[301];
-int cnt = 0;
-int ans = 0;
-std::priority_queue<std::tuple<int, int, int>, 
-    std::vector<std::tuple<int, int, int>>, 
-    std::greater<std::tuple<int, int, int>>> pq;
+int N;
+std::vector<std::pair<int, int>> adj_list[301]; //비용, 노드
+
+bool vis[301];
+std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
 
 int main(void) {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    std::cin >> n;
-    v = n + 1;
-    for(int i = 0; i < n; i++) {
-        int w;
-        std::cin >> w;
-        adj[i].push_back({w, n});
-        adj[n].push_back({w, i});
+    std::cin >> N;
+    int cost;
+    for(int i = 1; i <= N; i++) { //새 우물을 파는건 0번째 우물에서 물을 끌어오는 것으로 간주한다
+        std::cin >> cost;
+        adj_list[0].push_back({cost, i});
+        adj_list[i].push_back({cost, 0});
     }
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            int w;
-            std::cin >> w;
-            if(i == j) continue;
-            adj[i].push_back({w, j});
+
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= N; j++) {
+            std::cin >> cost;
+            if(i >= j) continue;
+
+            adj_list[i].push_back({cost, j});
+            adj_list[j].push_back({cost, i});
         }
     }
 
-    check[0] = true;
-    for(auto next : adj[0]) {
-        pq.push({next.first, 0, next.second});
-    }
-
-    while(cnt < v - 1) {
-        int cost, a, b;
-        std::tie(cost, a, b) = pq.top();
+    vis[0] = true;
+    for(auto& p : adj_list[0]) pq.push(p);
+    int cnt = 0;
+    int ans = 0;
+    while(!pq.empty()) {
+        std::pair<int, int> cur = pq.top();
         pq.pop();
-        if(check[b]) continue;;
-        check[b] = true;
+        if(vis[cur.second]) continue;
+
+        vis[cur.second] = true;
         cnt++;
-        ans += cost;
-        for(auto next : adj[b]) {
-            if(!check[next.second]) pq.push({next.first, b, next.second});
-        }
+        ans += cur.first;
+        if(cnt == N) break; //0번째 우물을 포함하므로 간선의 갯수는 N
+
+        for(auto& p : adj_list[cur.second]) if(!vis[p.second]) pq.push(p);
     }
 
     std::cout << ans;
--- baek/minimum spanning tree/minimum spanning tree.cpp
+++ baek/minimum spanning tree/minimum spanning tree.cpp
@@ -1,45 +1,52 @@
 //1197
 #include <bits/stdc++.h>
 
-std::vector<int> p(10005, -1);
-int find(int x) {
-    if(p[x] < 0) return x;
-    return p[x] = find(p[x]);
+int V, E;
+std::tuple<int, int, int> edge[100000]; //비용, u, v
+
+int par[10001]; //-1로 초기화 하고 부모 노드를 가리킴, 음수일 경우 그 절대값이 트리의 높이
+
+int find(int v) {
+    if(par[v] < 0) return v; //끝에 도달하면 루트 반환
+    return par[v] = find(par[v]); //경로 압축
 }
 
-bool is_diff_group(int u, int v) {
-    u = find(u); v = find(v);
-    if(u == v) return 0;
-    if(p[u] == p[v]) p[u]--;
-    if(p[u] < p[v]) p[v] = u;
-    else p[u] = v;
-    return 1;
-}
+bool group_check(int u, int v) {
+    int group_u = find(u);
+    int group_v = find(v);
+    if(group_u == group_v) return true; //같은 그룹
 
-int v, e;
-std::tuple<int, int, int> edges[100001];
+    if(par[group_u] == par[group_v]) par[group_u]--; //부모가 같은 경우 어느 한쪽의 트리 높이를 높여 합친다
+
+    if(par[group_u] < par[group_v]) par[group_v] = group_u;
+    else par[group_u] = group_v; //높은 트리 아래에 작은 트리를 합친다
+
+    return false;
+}
 
 int main(void) {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    std::cin >> v >> e;
-    for(int i = 0; i < e; i++) {
-        int a, b, cost;
-        std::cin >> a >> b >> cost;
-        edges[i] = {cost, a, b};
+    std::cin >> V >> E;
+    int u, v, cost;
+    for(int i = 0; i < E; i++) {
+        std::cin >> u >> v >> cost;
+        edge[i] = {cost, u, v};
     }
-    std::sort(edges, edges + e);
+    std::sort(edge, edge + E);
 
+    std::fill(par, par + V + 1, -1);
     int cnt = 0;
     int ans = 0;
-    for(int i = 0; i < e; i++) {
-        int a, b, cost;
-        std::tie(cost, a, b) = edges[i];
-        if(!is_diff_group(a, b)) continue;
+    for(int i = 0; i < E; i++) {
+        std::tie(cost, u, v) = edge[i];
+        if(group_check(u, v)) continue;
+
         ans += cost;
         cnt++;
-        if(cnt == v - 1) break;
+        if(cnt == V - 1) break;
     }
+
     std::cout << ans;
 }
