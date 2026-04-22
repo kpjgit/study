490864dbc4418646651c58ad913caa3c2c1b3847
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:41:10 +0900

    [Silver II] Title: 좌표 압축, Time: 468 ms, Memory: 18348 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+int N;
+int arr[1000000];
+
+std::vector<int> table;
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N;
+    std::vector<int> temp;
+    for(int i = 0; i < N; i++) {
+        std::cin >> arr[i];
+        temp.push_back(arr[i]);
+    }
+    std::sort(temp.begin(), temp.end());
+
+    table.push_back(*temp.begin());
+    for(auto i : temp) {
+        if(table.back() == i) continue;
+
+        table.push_back(i);
+    } //table.erase(std::unique(table.begin(), table.end()), table.end());
+
+    for(int i = 0; i < N; i++) std::cout << std::lower_bound(table.begin(), table.end(), arr[i]) - table.begin() << ' ';
+}
