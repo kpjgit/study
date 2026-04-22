88ab6401c6a7d69d030cf44aaa43c7c2c2fa5537
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:49:43 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int N;
+int arr[1000001];
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N;
+
+    arr[1] = 0;
+    for(int i = 2; i <= N; i++) {
+        arr[i] = arr[i - 1] + 1;
+        if(i % 2 == 0) arr[i] = std::min(arr[i], arr[i / 2] + 1);
+        if(i % 3 == 0) arr[i] = std::min(arr[i], arr[i / 3] + 1);
+    }
+
+    std::cout << arr[N];
+}
