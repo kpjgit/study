945eaceccf7e91b1480b61b08953b16001417f6d
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:25:01 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 208 ms, Memory: 21316 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+int N, M, K, X;
+std::vector<int> adj[300001];
+
+int dist[300001];
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    std::cin >> N >> M >> K >> X;
+    int u, v;
+    for(int i = 0; i < M; i++) {
+        std::cin >> u >> v;
+        adj[u].push_back(v);
+    }
+    
+    std::vector<int> ans;
+    std::fill(dist + 1, dist + N + 1, -1);
+    std::queue<int> q;
+    q.push(X);
+    dist[X] = 0;
+    while(!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        
+        for(int nxt : adj[cur]) {
+            if(dist[nxt] != -1) continue;
+            
+            dist[nxt] = dist[cur] + 1;
+            
+            if(dist[nxt] == K) ans.push_back(nxt);
+            else q.push(nxt);
+        }
+    }
+    
+    std::sort(ans.begin(), ans.end());
+    for(int a : ans) std::cout << a << '\n';
+    if(ans.size() == 0) std::cout << -1;
+}
