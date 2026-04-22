9208b67ed4c8bb8ea4cf6d81fa735f67d02c102e
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:45:45 +0900

    [Silver IV] Title: 로프, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+int N;
+int arr[100000];
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N;
+    for(int i = 0; i < N; i++) std::cin >> arr[i];
+    std::sort(arr, arr + N);
+
+    int prev = 0;
+    for(int i = N - 1; i >= 0; i--) {
+        int num = N - i;
+        if(arr[i] * num >= prev) prev = arr[i] * num;
+    }
+
+    std::cout << prev;
+}
