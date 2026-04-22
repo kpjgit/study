378fea6c7eca049b36f2ac6ae2b4262e657b924c
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:32:41 +0900

    [Gold IV] Title: 플로이드, Time: 20 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+int n, m;
+int table[101][101];
+
+const int inf = 10000001;
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> n >> m;
+    for(int i = 1; i <= n; i++) {std::fill(
+        table[i] + 1, table[i] + n + 1, inf);
+        table[i][i] = 0;
+    }
+
+    int u, v, cost;
+    for(int i = 0; i < m; i++) {
+        std::cin >> u >> v >> cost;
+        table[u][v] = std::min(table[u][v], cost);
+    }
+
+    for(int route = 1; route <= n; route++) {
+        for(int st = 1; st <= n; st++) {
+            for(int en = 1; en <= n; en++) {
+                table[st][en] = std::min(table[st][en], table[st][route] + table[route][en]);
+            }
+        }
+    }
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(table[i][j] == inf) std::cout << "0 ";
+            else std::cout << table[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+}
