903c186a8a6d8cc7122bf3471c26fc7bd159a1b0
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:39:18 +0900

    [Silver IV] Title: 수들의 합 2, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+int N, M;
+int arr[10000];
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N >> M;
+    for(int i = 0; i < N; i++) std::cin >> arr[i];
+
+    int st = 0;
+    int en = 0;
+    int ans = 0;
+    int sum = arr[0];
+    while(st < N && en < N) {
+        if(sum < M) {
+            en++;
+            sum += arr[en];
+        } else {
+            if(sum == M) ans++;
+            sum -= arr[st];
+            st++;
+        }
+    }
+
+    std::cout << ans;
+}
