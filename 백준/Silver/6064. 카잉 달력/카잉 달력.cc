d812a3a58ab60cb3b0339bfd015ee3e1deb70255
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:43:38 +0900

    [Silver I] Title: 카잉 달력, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+int T, M, N, x, y; //x, y는 M, N으로 나눈 나머지
+
+int gcd(int a, int b) {
+    if(a == 0) return b;
+    return gcd(b % a, a);
+}
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+int func(int M, int N, int x, int y) {
+    if(x == M) x = 0;
+    if(y == N) y = 0;
+
+    int l = lcm(M, N);
+    for(int i = x; i <= l; i += M) {
+        if(i == 0) continue;
+        if(i % N == y) return i;
+    }
+
+    return -1;
+}
+
+int main(void){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> T;
+    for(int num = 0; num < T; num++) {
+        std::cin >> M >> N >> x >> y;
+        std::cout << func(M, N, x, y) <<'\n';
+    }
+}
