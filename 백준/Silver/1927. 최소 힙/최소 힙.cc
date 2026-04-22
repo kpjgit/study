e042b04ed2d6babf9824e45b2d4f61b4fcf86f50
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:37:22 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+int N;
+std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N;
+    int x;
+    for(int i = 0; i < N; i++) {
+        std::cin >> x;
+        if(x == 0) {
+            if(pq.empty()) {
+                std::cout << 0 << '\n';
+                continue;
+            }
+
+            std::cout << pq.top() << '\n';
+            pq.pop();
+        } else pq.push(x);
+    }
+}
