4462065ce242afada8c16711f46c4f50b9218f02
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:50:46 +0900

    [Silver III] Title: 시리얼 번호, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+int N;
+std::vector<std::string> guitar;
+
+bool cmp(const std::string& a, const std::string& b) {
+    if(a.size() != b.size()) return a.size() < b.size();
+    else {
+        int sum_a = 0;
+        int sum_b = 0;
+
+        for(int i = 0; i < a.size(); i++) {
+            if(a[i] > '0' && a[i] <= '9') sum_a += a[i] - '0';
+            if(b[i] > '0' && b[i] <= '9') sum_b += b[i] - '0';
+        }
+
+        if(sum_a < sum_b) return true;
+        else if(sum_a > sum_b) return false; 
+    }
+
+    return a < b;
+}
+
+int main(void) {
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    std::cin >> N;
+    for(int i = 0; i < N; i++) {
+        std::string s;
+        std::cin >> s;
+        guitar.push_back(s);
+    }
+
+    std::sort(guitar.begin(), guitar.end(), cmp);
+    for(int i = 0; i < N; i++) std::cout << guitar[i] << '\n';
+}
