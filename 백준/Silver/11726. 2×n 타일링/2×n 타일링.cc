99569259723bb6cb77dab79f6a0ece27be1e9991
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:49:12 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int n;
+
+int arr[1000];
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> n;
+
+    arr[0] = 1;
+    arr[1] = arr[0] + 1;
+    arr[2] = arr[1] + arr[0];
+    
+    for(int i = 2; i < n; i++) arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+
+    std::cout << arr[n - 1];
+}
