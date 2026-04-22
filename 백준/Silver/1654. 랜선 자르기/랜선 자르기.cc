f85052f65a213848b6ea6001479b1614aba8e3e6
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:40:56 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+int K, N;
+int arr[10000];
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> K >> N;
+    long long st = 1;
+    long long en = 0x7fffffff;
+    for(int i = 0; i < K; i++) std::cin >> arr[i];
+
+    while(st < en) {
+        long long mid = (st + en + 1) / 2;
+        
+        int cnt = 0;
+        for(int i = 0; i < K; i++) cnt += arr[i] / mid;
+
+        if(cnt >= N) st = mid;
+        else en = mid - 1;
+    }
+    
+    std::cout << st;
+}
