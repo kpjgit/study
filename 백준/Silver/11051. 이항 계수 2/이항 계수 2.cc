2791017fc5351ffbdf9c9b3840eb8d97c8be486e
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:43:28 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int N, K;
+int arr[1001][1001]; //arr[i][j] iCj의 값
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    for(int i = 1; i <= 1000; i++) {
+        arr[i][0] = 1;
+        arr[i][i] = 1;
+        for(int j = 1; j < i; j++) {
+            arr[i][j] = (arr[i - 1][j] + arr[i - 1][j - 1]) % 10007;
+        }
+    }
+
+    std::cin >> N >> K;
+    std::cout << arr[N][K];
+}
