2fe95ca7150c1dc7a2a6dad2d7f51439a82e3744
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:48:48 +0900

    [Gold V] Title: 1로 만들기 2, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+int N;
+
+int arr[1000001];
+int route[1000001];
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N;
+
+    arr[1] = 0;
+    for(int i = 2; i <= N; i++) {
+        arr[i] = arr[i - 1];
+        route[i] = i - 1;
+
+        if(i % 2 == 0) {
+            if(arr[i] > arr[i / 2]) {
+                arr[i] = arr[i / 2];
+                route[i] = i / 2;
+            }
+        }
+
+        if(i % 3 == 0) {
+            if(arr[i] > arr[i / 3] ) {
+                arr[i] = arr[i / 3];
+                route[i] = i / 3;
+            }
+        }
+
+        arr[i]++;
+    }
+
+    std::cout << arr[N] << '\n';
+    int idx = N;
+    std::cout << N << ' ';
+    for(int i = 0; i < arr[N]; i++) {
+        std::cout << route[idx] << ' ';
+        idx = route[idx];
+    }
+}
