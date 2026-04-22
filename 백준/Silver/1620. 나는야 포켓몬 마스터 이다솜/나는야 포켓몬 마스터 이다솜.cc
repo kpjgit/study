e5a2097a862ac21cc5dede59d9d54b01b3b6cf4b
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:38:39 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 108 ms, Memory: 20660 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+int N, M;
+std::string arr[100001];
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N >> M;
+    std::unordered_map<std::string, int> dic;
+    std::string s;
+    for(int i = 1; i <= N; i++) {
+        std::cin >> s;
+        dic[s] = i;
+        arr[i] = s;
+    }
+
+    for(int i = 0; i < M; i++) {
+        std::cin >> s;
+        if(s[0] >= '0' && s[0] <= '9') std::cout << arr[std::stoi(s)] << '\n';
+        else std::cout << dic[s] << '\n';
+    }
+}
