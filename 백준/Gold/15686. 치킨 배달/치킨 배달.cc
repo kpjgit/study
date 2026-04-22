45cd98491946d201812dd16f07e4c7846b92a818
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:53:28 +0900

    [Gold V] Title: 치킨 배달, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+int N, M;
+
+std::vector<std::pair<int, int>> chicken;
+std::vector<std::pair<int, int>> house;
+
+int main(void){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int input;
+    std::cin >> N >> M;
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            std::cin >> input;
+            if(input == 1) house.push_back({i, j});
+            if(input == 2) chicken.push_back({i, j});
+        }
+    }
+
+    std::vector<bool> brute(chicken.size(), true);
+    std::fill(brute.begin(), brute.begin() + chicken.size() - M, false); //false는 폐업
+    
+    int ans = 10000;
+    do {
+        int dist = 0;
+        for(std::pair<int, int> h : house) {
+            int temp = 100;
+
+            for(int i = 0; i < chicken.size(); i++) {
+                if(brute[i] == false) continue;
+                temp = std::min(temp, std::abs(chicken[i].first - h.first) + std::abs(chicken[i].second - h.second));
+            }
+            dist += temp;
+        }
+        ans = std::min(ans, dist);
+    } while(std::next_permutation(brute.begin(), brute.end()));
+
+    std::cout << ans;
+}
