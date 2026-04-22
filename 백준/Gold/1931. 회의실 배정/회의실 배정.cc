d311316beeff4f5d3c107a7dfdb84633b60e53cb
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:45:52 +0900

    [Gold V] Title: 회의실 배정, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+int N;
+std::pair<int, int> arr[100001];//시작, 끝
+
+bool cmp(const std::pair<int, int>& a, const std::pair<int, int>& b) {
+    if(a.second != b.second) return a.second < b.second;
+    return a.first < b.first;
+}
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N;
+    for(int i = 0; i < N; i++) {
+        int st, en;
+        std::cin >> st >> en;
+        arr[i] = {st, en};
+    }
+    std::sort(arr, arr + N, cmp);
+
+    int ans = 0;
+    int temp = -1;
+    for(int i = 0; i < N; i++) {
+        if(arr[i].first >= temp) {
+            temp = arr[i].second;
+            ans++;
+        }
+    }
+
+    std::cout << ans;
+}
