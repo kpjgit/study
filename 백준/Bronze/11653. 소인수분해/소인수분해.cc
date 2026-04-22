b1be199ea4d3fd8e958773594700f4554340fe02
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:43:45 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+int N;
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N;
+    std::vector<int> primes;
+    int i = 2;
+    while(i * i <= N) {
+        if(N % i == 0) {
+            N /= i;
+            primes.push_back(i);
+        } else i++;
+    }
+    if(N > 1) primes.push_back(N);
+
+    for(int i : primes) std::cout << i << '\n';
+}
