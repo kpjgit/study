84baf42688dcdedc1d0b2ef96849223eda0df302
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:43:58 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+int N;
+
+bool func(int a) {
+    if(a == 1) return false;
+
+    for(int i = 2; i * i <= a; i++) {
+        if(a % i == 0) return false;
+    }
+
+    return true;
+}
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int ans = 0;
+    std::cin >> N;
+    for(int i = 0; i < N; i++) {
+        int temp;
+        std::cin >> temp;
+
+        if(func(temp)) ans++;
+    }
+
+    std::cout << ans;
+}
