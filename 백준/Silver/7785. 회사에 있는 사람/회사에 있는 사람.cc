e4343182f845164a2ff701de5f866819769758c5
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:38:45 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 84 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+int n;
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> n;
+    std::unordered_set<std::string> employee;
+    std::string name, input;
+    for(int i = 0; i < n; i++) {
+        std::cin >> name >> input;
+        if(input == "enter") employee.insert(name);
+        else employee.erase(name);
+    }
+
+    std::vector<std::string> v(employee.begin(), employee.end());
+    std::sort(v.begin(), v.end(), std::greater<std::string>());
+    for(auto& s : v) std::cout << s << '\n';
+}
