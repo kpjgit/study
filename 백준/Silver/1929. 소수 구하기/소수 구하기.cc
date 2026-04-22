260b58e00d59a63a4e9c3e60a62d52f0ff6a59b7
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:43:53 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 3036 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+int M, N;
+
+std::vector<int> func(int n) {
+    std::vector<int> primes;
+    std::vector<bool> v(n + 1, true);
+    v[1] = false;
+
+    for(int i = 2; i * i <= n; i++) {
+        if(v[i] == false) continue;
+        
+        for(int j = i * i; j <= n; j += i) v[j] = false;
+    }
+
+    for(int i = 2; i <= n; i++) if(v[i]) primes.push_back(i);
+
+    return std::move(primes);
+}
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> M >> N;
+    std::vector<int> primes = func(N);
+
+    for(int i : primes) {
+        if(i >= M) std::cout << i << '\n';
+    }
+}
