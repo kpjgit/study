6005eef11268dbec73d111a39e103c5bfde367f1
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:43:32 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int N, K;
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N >> K;
+    int ans = 1;
+    for(int i = N; i > N - K; i--) ans *= i;
+    for(int i = 2; i <= K; i++) ans /= i;
+    std::cout << ans;
+}
