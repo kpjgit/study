1cf0bb3a568698e64f9ba79cebc6aa26ebe064eb
kpjgit <65382057+kpjgit@users.noreply.github.com>
2026-04-22 20:39:33 +0900

    [Gold V] Title: 수 고르기, Time: 20 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+int N, M;
+int A[100000];
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cin >> N >> M;
+    for(int i = 0; i < N; i++) std::cin >> A[i];
+    std::sort(A, A + N);
+
+    int st = 0;
+    int en = 0;
+    int ans = 2000000000;
+    while(st < N && en < N) {
+        if(A[en] - A[st] < M) en++;
+        else {
+            ans = std::min(ans, A[en] - A[st]);
+            st++;
+        }
+    }
+
+    std::cout << ans;
+}
